b3f468b94fab1721f04799945f6553938198afb0
0x47root <0x47root@protonmail.com>
2021-03-03 12:20:02 +0100

    Bestandsnamen hernoemd.

--- portscanner2.py
+++ portscanner2.py
@@ -1,34 +1,16 @@
 import socket
-import threading
-from queue import Queue
-
-print_lock = threading.Lock()
-target = '192.168.178.237'
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = '192.168.178.237'
 
 def portscan(port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        con = s.connect((target, port))
-        with print_lock:
-            print(f"Port {port} is open!")
-        con.close()
+        s.connect((server,port))
+        return True
     except:
-        pass
+        return False
 
-def threader():
-    while True:
-        worker = q.get()
-        portscan(worker)
-        q.task_done()
-
-q = Queue()
-
-for x in range(30):
-    t = threading.Thread(target=threader)
-    t.daemon = True
-    t.start()
-
-for worker in range(70,81):
-    q.put(worker)
-
-q.join()
+for x in range(70,81):
+    if portscan(x):
+        print(f"Port {x} is open!")
+    else:
+        print(f"Port {x} is closed.")
