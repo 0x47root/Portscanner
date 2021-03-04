9e3386a6444f2b643e9f07ecfe80769b3aece29a
0x47root <0x47root@protonmail.com>
2021-03-04 17:00:55 +0100

    UDP Scanner werkt nu ook :)

--- portscanner_niveau1.py
+++ portscanner_niveau1.py
@@ -1,4 +1,6 @@
+from scapy.all import *
 import socket
+import time
 
 def TCP_connect_scan(target, first_port, last_port):
     last_port += 1
@@ -11,17 +13,13 @@ def TCP_connect_scan(target, first_port, last_port):
             print(f"Port {port} is filtered or closed.")
 
 def UDP_scan(target, first_port, last_port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    last_port += 1
     for port in range(first_port, last_port):
-        try:
-            data = "Hello"
-            s.sendto(data, (target, port))
-            s.settimeout(0)
-            print((s.recvfrom(1024)))
-            break
-        except:
-            print("UDP scan error")
+        ans = sr1(IP(dst=target) / UDP(dport=port), timeout=5, verbose=0)
+        #time.sleep(1)
+        if ans == None:
+            print(f"Port {port} is open!")
+        else:
+            print(f"Port {port} is closed")
 
 #TCP_connect_scan('192.168.11.130', 1330, 1400)
-UDP_scan("192.168.11.130", 1332, 1334)
+UDP_scan("131.100.154.250", 52, 55)
