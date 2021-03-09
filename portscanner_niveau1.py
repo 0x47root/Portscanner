7e4e3965ad6405b93ba560d395b38763830d00b1
0x47root <0x47root@protonmail.com>
2021-03-09 16:50:23 +0100

    Alle scans geupdate en ze werken nu zoals behoren, behalve de UDP scan.

--- portscanner_niveau1.py
+++ portscanner_niveau1.py
@@ -1,18 +1,21 @@
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
-import socket
 import time
 
 def TCP_connect_scan(target, first_port, last_port):
     last_port += 1
+    s_port = RandShort()
     for port in range(first_port, last_port):
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((target,port))
-            print(f"Port {port} is open!")
-        except:
-            print(f"Port {port} is filtered or closed.")
+        ans = sr1(IP(dst=target)/TCP(sport=s_port, dport=port,flags="S"), timeout=1, verbose=0)
+        if ans == None:
+            print(f"Port {port} is closed")
+        elif ans.haslayer(TCP):
+            if ans.getlayer(TCP).flags == 0x12: # 0x12 = 18 = 16 + 2 = ACK + SYN
+                rst = sr(IP(dst=target)/TCP(sport=s_port, dport=port,flags="AR"), timeout = 1, verbose=0)
+                print(f"Port {port} is open!")
+            elif ans.getlayer(TCP).flags == 0x14: # 0x14 = 20 = 16 + 4 = ACK + RST
+                print(f"Port {port} is closed")
 
 def UDP_scan(target, first_port, last_port):
     last_port += 1
@@ -31,32 +34,35 @@ def UDP_scan(target, first_port, last_port):
 def TCP_SYN_scan(target, first_port, last_port):
     last_port += 1
     for port in range(first_port, last_port):
-        ans = sr1(IP(dst=target)/TCP(dport=port), timeout=1, verbose=0)
+        ans = sr1(IP(dst=target)/TCP(dport=port,flags="S"), timeout=1, verbose=0)
         # If the timeout value is not specified when this function is used against a unresponsive host,
         # the function will continue indefinitely
         if ans == None:
             print(f"Port {port} is filtered!")
-        else:
-            if int(ans[TCP].flags) == 18: # 18 means SYN (2) + ACK (16)
+        elif ans.haslayer(TCP):
+            if ans.getlayer(TCP).flags == 0x12: # 0x12 = 18 = 16 + 2 = ACK + SYN
+                rst = sr(IP(dst=target)/TCP(dport=port,flags="R"), timeout = 1, verbose=0)
                 print(f"Port {port} is open!")
-            #elif (int(ans.getlayer(ICMP).type) == 3 and int(ans.getlayer(ICMP).code) in [1, 2, 3, 9, 10, 13]):
-                #print(f"Port {port} is filtered!")
-            else:
+            elif ans.getlayer(TCP).flags == 0x14: # 0x12 = 20 = 16 + 4 = ACK + RST
                 print(f"Port {port} is closed")
+            elif ans.haslayer(ICMP):
+                if int(ans.getlayer(ICMP).type)==3 and int(ans.getlayer(ICMP).code) in [1,2,3,9,10,13]:
+                    print(f"Port {port} is filtered!")
 
 def XMAS_scan(target, first_port, last_port):
     last_port += 1
     for port in range(first_port, last_port):
-        ans = sr1(IP(dst=target)/TCP(dport=port, flags='FPU'), timeout=2, verbose=0) # PSH, FIN and URG
+        ans = sr1(IP(dst=target)/TCP(dport=port,flags="FPU"), timeout=1, verbose=0) # FIN, PSH and URG
         if ans == None:
-            print(f"Port {port} is open!")
+            print(f"Port {port} is filtered or open!")
         elif ans.haslayer(TCP):
-            if int(ans[TCP].flags) == 20: # 20 means RST (4) + ACK (16)
-                print(f"Port {port} is closed")
-            else:
-                print(f"Port {port} is filtered")
+            if ans.getlayer(TCP).flags == 0x14: # 0x14 = 20 = 16 + 4 = ACK + RST
+                print(f"Port {port} is closed.")
+            elif ans.haslayer(ICMP):
+                if int(ans.getlayer(ICMP).type)==3 and int(ans.getlayer(ICMP).code) in [1,2,3,9,10,13]:
+                    print(f"Port {port} is filtered.")
 
-#TCP_connect_scan('45.33.32.156', 514, 514)
-#UDP_scan('45.33.32.156', 120, 125)
+#TCP_connect_scan('192.168.178.1', 1, 100)
+UDP_scan('45.33.32.156', 1, 100)
 #TCP_SYN_scan('45.33.32.156', 514, 514)
-XMAS_scan('45.33.32.156', 20, 80)
+#XMAS_scan('45.33.32.156', 20, 80)
