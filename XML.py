0d2f8293b43c1ec2f9545175bc11900156cb5777
0x47root <0x47root@protonmail.com>
2021-03-30 10:53:40 +0200

    Added the functionality to safe the scanned ports to a dictionary. Also created separate files to safe the scan results in XML and JSON format.

--- XML.py
+++ XML.py
@@ -1,17 +1,10 @@
 import xml.etree.ElementTree as ET
 
-target = "45.33.32.156"
-
-# variables collecting scanned ports
-open = [21,80,443]
-closed = [1,2,3,4,5,6,7,8,9,10]
-filtered = [11,12,13]
-filtered_or_open = [1337]
-filtered_or_closed = [889,887]
+portscan = {'host': '45.33.32.156', 'open_ports': [22, 80], 'closed_ports': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100], 'filtered_ports': [], 'filtered_or_open_ports': [], 'filtered_or_closed_ports': []}
 
 # creating XML file layout
 root = ET.Element("ScanResults")
-host = ET.SubElement(root, "host", {"ip": target})
+host = ET.SubElement(root, "host", {"ip": portscan["host"]})
 open_ports = ET.SubElement(host, "open_ports")
 closed_ports = ET.SubElement(host, "closed_ports")
 filtered_ports = ET.SubElement(host, "filtered_ports")
@@ -26,10 +19,12 @@ def fillXML(port_list, port_element):
             port.text = str(p)
 
 # filling XML files with ports
-fillXML(open, open_ports)
-fillXML(closed, closed_ports)
-fillXML(filtered, filtered_ports)
-fillXML(filtered_or_open, filtered_or_open_ports)
-fillXML(filtered_or_closed, filtered_or_closed_ports)
+fillXML(portscan["open_ports"], open_ports)
+fillXML(portscan["closed_ports"], closed_ports)
+fillXML(portscan["filtered_ports"], filtered_ports)
+fillXML(portscan["filtered_or_open_ports"], filtered_or_open_ports)
+fillXML(portscan["filtered_or_closed_ports"], filtered_or_closed_ports)
 
-ET.dump(root)
+# writing XML file
+tree = ET.ElementTree(root)
+tree.write("portscan.xml")
--- portscanner_niveau1.py
+++ portscanner_niveau1.py
@@ -7,7 +7,7 @@ import time
 # suppress scapy warning message when importing module
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
-# create a nice banner for script
+# create a nice banner for CLI script
 print(pyfiglet.figlet_format("PORT SCANNER"))
 print(70 * "-")
 print("By 0x47root")
@@ -19,6 +19,15 @@ target = input("Please specify the IP-address to scan: ")
 first_port = int(input("Please specify the first port in port range: "))
 last_port = int(input("Please specify the last port in port range: ")) + 1
 
+# creating dictionary to store results
+portscan = {}
+portscan['host'] = target
+portscan['open_ports'] = []
+portscan['closed_ports'] = []
+portscan['filtered_ports'] = []
+portscan['filtered_or_open_ports'] = []
+portscan['filtered_or_closed_ports'] = []
+
 # define TCP-connect scan
 def TCP_connect_scan(target, first_port, last_port):
     for port in range(first_port, last_port):
@@ -26,9 +35,13 @@ def TCP_connect_scan(target, first_port, last_port):
         try:
             s.connect((target, port))
             print(f"Port {port} is open!!!!!!!!!!")
+            # writing results to dictionary
+            portscan['open_ports'].append(port)
+            # closing connection
             s.close()
         except:
             print(f"Port {port} is filtered or closed")
+            portscan['filtered_or_closed_ports'].append(port)
 
 # define UDP scan
 def UDP_scan(target, first_port, last_port):
@@ -37,14 +50,18 @@ def UDP_scan(target, first_port, last_port):
         time.sleep(1) # sleep one second to prevent false positives
         if res == None:
             print(f"Port {port} is open or filtered")
+            portscan['filtered_or_open_ports'].append(port)
         else:
             if res.haslayer(ICMP):
                 if int(res.getlayer(ICMP).type) == 3 and int(res.getlayer(ICMP).code) == 3:
                     print(f"Port {port} is closed")
+                    portscan['closed_ports'].append(port)
                 elif int(res.getlayer(ICMP).type) == 3 and int(res.getlayer(ICMP).code) in [1, 2, 9, 10, 13]:
                     print(f"Port {port} is filtered")
+                    portscan['filtered_ports'].append(port)
             elif res.haslayer(UDP):
                 print(f"Port {port} is open!!!!!!!!!!")
+                portscan['open_ports'].append(port)
 
 # define TCP-SYN scan
 def TCP_SYN_scan(target, first_port, last_port):
@@ -54,15 +71,19 @@ def TCP_SYN_scan(target, first_port, last_port):
         # the function will continue indefinitely
         if res == None:
             print(f"Port {port} is filtered")
+            portscan['filtered_ports'].append(port)
         elif res.haslayer(TCP):
             if res.getlayer(TCP).flags == 0x12: # 0x12 = 18 = 16 + 2 = ACK + SYN
                 rst = sr(IP(dst=target)/TCP(dport=port,flags="R"), timeout = 1, verbose=0)
                 print(f"Port {port} is open!!!!!!!!!!")
+                portscan['open_ports'].append(port)
             elif res.getlayer(TCP).flags == 0x14: # 0x12 = 20 = 16 + 4 = ACK + RST
                 print(f"Port {port} is closed")
+                portscan['closed_ports'].append(port)
             elif res.haslayer(ICMP):
                 if int(res.getlayer(ICMP).type)==3 and int(res.getlayer(ICMP).code) in [1,2,3,9,10,13]:
                     print(f"Port {port} is filtered")
+                    portscan['filtered_ports'].append(port)
 
 # define XMAS scan
 def XMAS_scan(target, first_port, last_port):
@@ -70,12 +91,15 @@ def XMAS_scan(target, first_port, last_port):
         res = sr1(IP(dst=target)/TCP(dport=port,flags="FPU"), timeout=1, verbose=0) # FIN, PSH and URG
         if res == None:
             print(f"Port {port} is filtered or open!!!!!!!!!!")
+            portscan['filtered_or_open_ports'].append(port)
         elif res.haslayer(TCP):
             if res.getlayer(TCP).flags == 0x14: # 0x14 = 20 = 16 + 4 = ACK + RST
                 print(f"Port {port} is closed")
+                portscan['closed_ports'].append(port)
             elif res.haslayer(ICMP):
                 if int(res.getlayer(ICMP).type)==3 and int(res.getlayer(ICMP).code) in [1,2,3,9,10,13]:
                     print(f"Port {port} is filtered")
+                    portscan['filtered_ports'].append(port)
 
 # check which scan to conduct
 if scan_type == "-sT":
@@ -86,3 +110,5 @@ elif scan_type == "-sS":
     TCP_SYN_scan(target, first_port, last_port)
 elif scan_type == "-sX":
     XMAS_scan(target, first_port, last_port)
+
+print(portscan)
