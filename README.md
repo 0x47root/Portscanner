0caef9e6e6e9c3ef80afe2994abc74a2ec43e528
0x47root <0x47root@protonmail.com>
2021-03-31 12:47:24 +0200

    Added all level1 functionalities to the script. Also edited the README file.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Eindopdracht-Module-4
-Dit is de repo voor de Python opdracht van Module 4.
+# Portscanner
+This portscanner is created by **0x47root**.
+
+## Usage:
+```bash
+python3 portscanner_niveau1.py
+```
+
+## Possible scan types
+- TCP-Connect Scan (-sT)
+- UDP Scan (-sU)
+- SYN Scan (-sS)
+- XMAS Scan (-sX)
+
+## Additional functionalities
+- Scan range of ports
+- Save scan results to XML or JSON
+- Automatically save results to SQLite database
--- portscanner_niveau1.py
+++ portscanner_niveau1.py
@@ -1,25 +1,53 @@
+import xml.etree.ElementTree as ET
 from scapy.all import *
+import ipaddress
 import pyfiglet
-import logging
+import sqlite3
+import os.path
 import socket
 import time
+import json
 
-# suppress scapy warning message when importing module
-logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
-
-# create a nice banner for CLI script
+# create a banner for CLI script
 print(pyfiglet.figlet_format("PORT SCANNER"))
 print(70 * "-")
 print("By 0x47root")
 print(70 * "-")
 
-# ask user for input
+# ask user for scan type with input sanitization
 scan_type = input("Please enter scan type (-sT, -sU, -sS or -sX): ")
-target = input("Please specify the IP-address to scan: ")
-first_port = int(input("Please specify the first port in port range: "))
-last_port = int(input("Please specify the last port in port range: ")) + 1
+while scan_type not in ['-sT', '-sU', '-sS', '-sX']:
+    scan_type = input("Wrong input, please enter one of the given options (-sT, -sU, -sS or -sX): ")
 
-# input sanitization
+# ask user for target with input sanitization
+target = input("Please specify the IP-address to scan: ")
+while True:
+    try:
+        ipaddress.IPv4Address(target)
+        break
+    except:
+        target = input("Wrong input, please specify a correct IPv4 address: ")
+
+# ask user for ports to scan with input sanitization
+first_port = int(input("Please specify the first port in port range: "))
+while first_port not in range(1, 65535):
+    first_port = int(input("Wrong input, please specify a single port between 1 and 65535: "))
+last_port = int(input("Please specify the last port in port range: ")) + 1
+while last_port not in range(1, 65535):
+    last_port = int(input("Wrong input, please specify a single port between 1 and 65535: ")) + 1
+while last_port <= first_port:
+    last_port = int(input("Please enter a number that is equal or higher than the first: ")) + 1
+
+# ask user how to save the scan results with input sanitization
+output = input("Do you want to save the output to a file? (y/n): ")
+while output not in ['y', 'n']:
+    output = input("Please enter a 'y' or a 'n': ")
+if output == 'y':
+    save_output = input("Do you want to save the results to JSON or XML?: ")
+    while save_output not in ['JSON', 'XML', 'json', 'xml']:
+        save_output = input("Please enter 'JSON' or 'XML'?: ")
+else:
+    save_output = False
 
 # creating dictionary to store results
 portscan = {}
@@ -104,7 +132,7 @@ def XMAS_scan(target, first_port, last_port):
                     print(f"Port {port} is filtered")
                     portscan['filtered_ports'].append(port)
 
-# check which scan to conduct
+# check which scan to conduct and execute scan
 if scan_type == "-sT":
     TCP_connect_scan(target, first_port, last_port)
 elif scan_type == "-sU":
@@ -114,4 +142,89 @@ elif scan_type == "-sS":
 elif scan_type == "-sX":
     XMAS_scan(target, first_port, last_port)
 
-print(portscan)
+# defining function to write to XML
+def writeToXML():
+    # creating XML file layout
+    root = ET.Element("ScanResults")
+    host = ET.SubElement(root, "host", {"ip": portscan["host"]})
+    scan_type = ET.SubElement(host, "scan_type")
+    open_ports = ET.SubElement(host, "open_ports")
+    closed_ports = ET.SubElement(host, "closed_ports")
+    filtered_ports = ET.SubElement(host, "filtered_ports")
+    filtered_or_open_ports = ET.SubElement(host, "filtered_or_open_ports")
+    filtered_or_closed_ports = ET.SubElement(host, "filtered_or_closed_ports")
+
+    # writing scan type
+    scan = ET.SubElement(scan_type, "scan")
+    scan.text = portscan["scan_type"]
+
+    # create function to fill XML file with ports
+    def fillXML(port_list, port_element):
+        if port_list:
+            for p in port_list:
+                port = ET.SubElement(port_element, "port")
+                port.text = str(p)
+
+    # filling XML file with ports
+    fillXML(portscan["open_ports"], open_ports)
+    fillXML(portscan["closed_ports"], closed_ports)
+    fillXML(portscan["filtered_ports"], filtered_ports)
+    fillXML(portscan["filtered_or_open_ports"], filtered_or_open_ports)
+    fillXML(portscan["filtered_or_closed_ports"], filtered_or_closed_ports)
+
+    # writing XML file
+    tree = ET.ElementTree(root)
+    tree.write("portscan.xml")
+
+# defining function to write to JSON
+def writeToJSON():
+    with open("portscan.json", "w") as outfile:
+        json.dump(portscan, outfile)
+
+# writing scan results to XML or JSON if specified
+if save_output:
+    if save_output in ['xml', 'XML']:
+        writeToXML()
+    elif save_output in ['json', 'JSON']:
+        writeToJSON()
+
+# define function to write to SQlite databse
+def writeToSQLite():
+    # check if database file already exists. If not; create database and insert table:
+    if os.path.isfile('portscan.db'):
+        pass
+    else:
+        conn = sqlite3.connect("portscan.db")
+        # create cursor object
+        c = conn.cursor()
+        c.execute('''CREATE TABLE portscans (
+            host CHAR, 
+            scan_type CHAR, 
+            open_ports ENUM, 
+            closed_ports ENUM, 
+            filtered_ports ENUM, 
+            filtered_or_open_ports ENUM, 
+            filtered_or_closed_ports ENUM)''')
+        # commit changes and close connection
+        conn.commit()
+        conn.close()
+
+    # add the scan results to the table
+    conn = sqlite3.connect("portscan.db")
+    c = conn.cursor()
+    # create query to insert portscan results and use question marks for best practices against SQLi
+    query = f'INSERT INTO portscans VALUES (?, ?, ?, ?, ?, ?, ?)'
+    # execute query
+    c.execute(query, (
+        f'{portscan["host"]}',
+        f'{portscan["scan_type"]}',
+        f'{portscan["open_ports"]}',
+        f'{portscan["closed_ports"]}',
+        f'{portscan["filtered_ports"]}',
+        f'{portscan["filtered_or_open_ports"]}',
+        f'{portscan["filtered_or_closed_ports"]}'))
+    conn.commit()
+    conn.close()
+
+# write scan results to SQLite database:
+writeToSQLite()
